94398fb35d2bc649bb446b5bb32af7d14991519d
Dan Chiev <danchiev2@Gmail.com>
2020-10-06 21:13:32 -0400

    mealDB API calls

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
+
+var queryURL = "https://www.themealdb.com/api/json/v1/1/filter.php?i=" + ingredients + // Whatever api key is ;
+
 $.ajax({
-    url: "https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour+sugar&number=5&apiKey=427db252e7484fb8be64ae2392879b2e",
+    url: queryURL,
     method: "GET"
   }).then(function(response) {
     console.log(response);
+  // display top recipe
+  // var newRecipeImage = ("<img>").attr("src", response.meals[0].strMealThumb)
+  // var newRecipeName = ("<h3>").text(response.meals[0].strMeal)
 
+  // var idMeal = response.meals[0].idMeal
+
+
+  
+  /* $.ajax({
+    var queryURLRecipe = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772" + idMeal + // Whatever api key is ;
+    url: queryURLRecipe,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response); */
+
+    // var mealInstructions = ("<p>").text(response.meals[0].strInstructions)
+    // var foodCountry = ("<p>").text(response.meals[0].strArea);
+    // var ingredient1 = ("<li>").text(response.meals[0].strIngredient1)
+    // var ingredient2 = ("<li>").text(response.meals[0].strIngredient2)
+    // var ingredient3 = ("<li>").text(response.meals[0].strIngredient3)
+    // var ingredient4 = ("<li>").text(response.meals[0].strIngredient4)
+    // var ingredient5 = ("<li>").text(response.meals[0].strIngredient6)
 
     }
+
+
+ 
+
+  
+  
+  
 
   );
 
